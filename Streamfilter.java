425c487dfccc790c7d7da319d2dbee8833eb792b
axwinachu <ceaswin146@gmail.com>
2025-11-16 21:18:38 +0530

    After complete

---
+++ Streamfilter.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Streamfilter {
+    public static void main(String[] args) {
+        List<Integer> li =new ArrayList<>(Arrays.asList(10,20,45,5,665,7));
+        Stream<Integer> s=li.stream();
+
+        s.filter()
+        //
+    }
+}
